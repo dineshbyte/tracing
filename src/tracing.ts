b3d74b8599fd62a0a781a98c69b2307cfa7089e8
rsys-dinesh-kumar <dinesh@radiansys.com>
2024-09-05 09:57:50 +0530

    fix: change in tracing function

--- src/tracing.ts
+++ src/tracing.ts
@@ -1,21 +1,26 @@
+import {createNamespace} from 'cls-hooked';
 import {NextFunction, Request, Response} from 'express';
 import {v4 as uuidv4} from 'uuid';
-import {createNamespace} from 'cls-hooked';
 
 const session = createNamespace('BYTE_LOGGER');
 
-export const setCorrelationId = (cid: string): void => {
-    session.set('cid', cid);
-};
-
-export const getCorrelationId = (): string | undefined => {
-    return session.get('cid');
-};
-
+/**
+ * Middleware to generate a unique traceId for each request
+ * @param req
+ * @param res
+ * @param next
+ */
 export const requestTraceMiddleware = (req: Request, res: Response, next: NextFunction): void => {
-    const cid = uuidv4();
+    const traceId = uuidv4();
     session.run(() => {
-        setCorrelationId(cid);
+        session.set('traceId', traceId);
         next();
     });
+};
+
+/**
+ * Get traceId from the current session
+ */
+export const getTraceId = (): string | undefined => {
+    return session.get('traceId');
 };
